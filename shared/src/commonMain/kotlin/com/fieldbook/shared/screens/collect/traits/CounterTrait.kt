0ded1e56f5cbb10c00ebb5a743f04b8a045f965b
cuenyad <diego.cuenya@leafnode.io>
2026-05-22 14:20:26 -0300

    Refactor `CounterTrait` to replace `FilledIconButton` with `Button`, use `Text` for labels, and simplify layout.
    Issue: IBP-7142
    Reviewed-by: none

--- shared/src/commonMain/kotlin/com/fieldbook/shared/screens/collect/traits/CounterTrait.kt
+++ shared/src/commonMain/kotlin/com/fieldbook/shared/screens/collect/traits/CounterTrait.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,11 +13,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.fieldbook.shared.generated.resources.Res
-import com.fieldbook.shared.generated.resources.minus
-import com.fieldbook.shared.theme.FilledIconButton
-import org.jetbrains.compose.resources.painterResource
+import com.fieldbook.shared.theme.Button
 
 @Composable
 fun CounterTrait(
@@ -33,27 +30,20 @@ fun CounterTrait(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
-        FilledIconButton(
+        Button(
             onClick = { count--; onValueChange(count.toString()) },
-            shape = CircleShape
+            shape = CircleShape,
+            modifier = Modifier.size(84.dp)
         ) {
-            Icon(
-                painter = painterResource(Res.drawable.minus),
-                contentDescription = "Increment",
-                modifier = Modifier.size(38.dp)
-            )
+            Text("-1", color = Color.Black, fontWeight = FontWeight.Bold)
         }
         Spacer(Modifier.size(8.dp))
-        FilledIconButton(
+        Button(
             onClick = { count++; onValueChange(count.toString()) },
             shape = CircleShape,
             modifier = Modifier.size(156.dp),
         ) {
-            Icon(
-                imageVector = Icons.Filled.Add,
-                contentDescription = "Increment",
-                modifier = Modifier.size(38.dp)
-            )
+            Text("+1", color = Color.Black, fontWeight = FontWeight.Bold)
         }
     }
 }
